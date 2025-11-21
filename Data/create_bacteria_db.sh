9a91f04aeccf2c0deeb794de79839f32bb112298
Troy La'akea Zen Helenihi <troy430@rice-01.farmshare.sunet>
2025-11-20 18:01:27 -0800

    modify create_bacteria_db.sh to run properly

--- Data/create_bacteria_db.sh
+++ Data/create_bacteria_db.sh
@@ -7,7 +7,22 @@
 #SBATCH --mem=4G
 #SBATCH --time=04:00:00
 
-DATABASE="bacteria.db"
-$RUN python insert_gff_table.py --database_path $DATABASE
-$RUN python insert_protein_cluster_table.py --database_path $DATABASE
-$RUN python insert_metadata_table.py --database_path $DATABASE
+# Make sure logs dir exists
+mkdir -p logs
+
+# Adjust this path to wherever your repo actually is
+REPO_DIR=/scratch/users/$USER/repos/BIOS270-AU25
+SIF=${REPO_DIR}/Environment/bioinformatics_latest.sif
+DATA_DIR=${REPO_DIR}/Data
+DATABASE="${DATA_DIR}/bacteria.db"
+
+# Command to run things INSIDE the container
+RUN="apptainer run \
+  -B /farmshare/user_data/$USER,/farmshare/home/classes/bios/270,/scratch/users/$USER \
+  ${SIF}"
+
+cd "$DATA_DIR"
+
+$RUN python insert_gff_table.py --database_path "$DATABASE"
+$RUN python insert_protein_cluster_table.py --database_path "$DATABASE"
+$RUN python insert_metadata_table.py --database_path "$DATABASE"
